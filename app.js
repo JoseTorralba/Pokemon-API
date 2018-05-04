29106e839dcebeb6adf5475c77a46ba78bae80ca
JoseTorralba <31675524+JoseTorralba@users.noreply.github.com>
2018-05-03 23:39:30 -0400

    started on final

---
+++ app.js
@@ -0,0 +1,9 @@
+$(function() {
+    // JSON
+    var pokemonApiUrl = "http://pokeapi.co/api/v2/generation/1";
+
+    // data returns pokemon info
+    $.getJSON(pokemonApiUrl).done(function(data) {
+
+    });
+});
